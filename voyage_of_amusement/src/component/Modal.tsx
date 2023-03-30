43c2146f78157ce6adfd9bcdf76545e041f0b9f6
Yunze Zhao <zhao.yunzeabh@gmail.com>
2023-03-30 01:51:21 -0400

    add routes

---
+++ voyage_of_amusement/src/component/Modal.tsx
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+
+function Modal(props) {
+ console.log('render modal')
+  return (
+    <div>
+      {props.isOpen && (
+        <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity">
+          <div className="fixed inset-0 z-10 overflow-y-auto">
+            <div className="flex min-h-screen items-end justify-center p-4 text-center sm:items-center sm:p-0">
+              <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" onClick={() => {props.onClose()}}></div>
+              <div className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+                <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
+                  <div className="sm:flex sm:items-start">
+                    <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
+                      <h3 className="text-base font-semibold leading-6 text-gray-900" id="modal-title">{props.title}</h3>
+                      <div className="mt-2">
+                        <p className="text-sm text-gray-500">{props.description}</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+                  <button type="button" onClick={() => {console.log('confirm')}} className="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto">{props.confirmLabel}</button>
+                  <button type="button" onClick={() => {props.onClose()}} className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto">{props.cancelLabel}</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default Modal
---
+++ voyage_of_amusement/src/pages/attractions.tsx
@@ -0,0 +1,73 @@
+import { useRouter } from 'next/router';
+import Image from 'next/image';
+import React, { useState } from 'react';
+
+interface Attraction {
+    type: string;
+    name: string;
+    description: string;
+    options: string[];
+}
+
+const attractionlist: Attraction[] = [
+    {
+        name: 'Roller Coaster',
+        description:
+            'Experience the thrill of our roller coasters, with twists, turns, and drops that will leave you breathless!',
+        options: ['Option 1', 'Option 2', 'Option 3'],
+        image: 'https://source.unsplash.com/random/800x600?rollercoaster',
+    },
+    {
+        name: 'Parking',
+        description:
+            'Our parking lot offers ample space for your vehicle, with easy access to the park entrance. Plus, with our secure parking, you can have peace of mind while you enjoy your day.',
+        options: ['Option 1', 'Option 2', 'Option 3'],
+        image: 'https://source.unsplash.com/random/800x600?parking',
+    },
+    {
+        name: 'Shows',
+        description:
+            'Enjoy a variety of acts and performances, from music and dancing to acrobatics and comedy. You won\'t want to miss our shows!',
+        options: ['Option 1', 'Option 2', 'Option 3'],
+        image: 'https://source.unsplash.com/random/800x600?shows',
+    },
+    {
+        name: 'Stores',
+        description:
+            'Visit our store for a range of souvenirs, snacks, and gifts for you to take home and remember your day at the park.',
+        options: ['Option 1', 'Option 2', 'Option 3'],
+        image: 'https://source.unsplash.com/random/800x600?stores',
+    },
+    {
+        name: 'Restaurant',
+        description:
+            'Our restaurants offer a wide range of food options, from quick bites to sit-down meals. With options for all tastes and diets, you\'re sure to find something you love.',
+        options: ['Option 1', 'Option 2', 'Option 3'],
+        image: 'https://source.unsplash.com/random/800x600?restaurant',
+    },
+];
+
+const Attractions = () => {
+    const router = useRouter();
+    const { type } = router.query;
+    const [searchTerm, setSearchTerm] = useState('');
+
+    return (
+        <div className="flex flex-col items-center p-4 md:p-8">
+            <h1 className="text-3xl md:text-5xl font-bold mb-8">
+                {type} Attractions
+            </h1>
+            <div className="relative mb-5 w-full ml-5 mr-5 container">
+                <input
+                    type="email" id="floating_filled" className=" block rounded-t-md px-3 pb-2 pt-5 w-full text-sm text-gray-900 bg-gray-50 border-0 border-b-2 border-gray-100 appearance-none focus:outline-none focus:bg-indigo-50 focus:ring-0 focus:border-indigo-500 peer" placeholder=" " />
+                <label htmlFor="floating_filled" className="absolute text-sm text-gray-400 duration-300 transform -translate-y-4 scale-75 top-4 z-10 origin-[0] left-2.5 peer-focus:text-indigo-500  peer-focus:font-semibold peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4">
+                    Search...
+                </label>
+            </div>
+
+
+        </div>
+    );
+}
+
+export default Attractions
