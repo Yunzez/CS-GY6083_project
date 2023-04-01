ef3f1d775c2c6d6c080e04362568d995670cb542
Yunze Zhao <zhao.yunzeabh@gmail.com>
2023-03-31 22:46:16 -0400

    add procedure

--- procedure.sql
+++ procedure.sql
@@ -1,36 +1,57 @@
 CREATE PROCEDURE get_summary_data_by_date (@input_date DATE) AS
 BEGIN
     SELECT AA.Visitor_ID, (AA.Source_Type + '-' +
-        CAST((case
-                when AA.Source_Type = 'Tic'
-                    then AT.Ticket_ID
-                when AA.Facility_ID is not null
-                    then AP.Facility_ID
-                else AA.Activity_ID
-              END) AS VARCHAR(5))) AS Source_ID, AA.Source_Type,
-       source_Type_name AS Sore_Type, Payment_ID,
-       (case
-           when AA.Source_Type = 'Tic'
-               then AT.Price
-            when AA.Source_Type = 'Shw'
-                then SHOWS.Price
-            when AA.Source_Type = 'Sto'
-                then AP.Payment_Amount
-            when AA.Source_Type = 'Par'
-                then Amount_Due
-            else 0
-           end
-           ) as 'amount_due(Price)',
-       Cast(AA.Activity_Date as DATE) as Visit_date
+    CAST(
+        (case
+            when AA.Source_Type = 'Tic'
+            then AT.Ticket_ID
+            when AA.Facility_ID is not null
+            then AP.Facility_ID
+            else AA.Activity_ID
+            END
+        )
+        AS VARCHAR(5))) AS Source_ID, AA.Source_Type,
+    source_Type_name AS Sore_Type, Payment_ID,
+    (case
+        when AA.Source_Type = 'Tic'
+            then AT.Price
+        when AA.Source_Type = 'Shw'
+            then SHOWS.Price
+        when AA.Source_Type = 'Sto'
+            then AP.Payment_Amount
+        when AA.Source_Type = 'Par'
+            then Amount_Due
+        else 0
+    end
+    ) as price ,
+    (
+    case when (DATENAME(WEEKDAY, AA.Activity_Date) IN ('Saturday', 'Sunday')) -- weekend
+                    OR (EXISTS (SELECT * FROM AFZ_Holidays WHERE Holiday_Date = AA.Activity_Date)) -- holiday
+            then 1
+        when AA.Source_Type = 'Tic'
+            then ATT.Discount * ATM.Discount
+        WHEN AA.Source_Type = 'Shw'
+            then case when DATEDIFF(year, AV.Birthdate, getutcdate()) <= 7
+                    then 0 -- free show for child
+                else 1
+                end
+        else 1
+    end
+    ) as discount,
+    AP.Payment_Amount as "amount(after discount)",
+    Cast(AA.Activity_Date as DATE) as Visit_date
         FROM AFZ_Activity AA
         JOIN AFZ_Payment AP on AA.Activity_ID = AP.Activity_ID
         LEFT JOIN AFZ_Tickets AT ON AT.Activity_ID = AP.Activity_ID
-        JOIN AFZ_Source_Type AST on AA.Source_Type = AST.Source_Type
-        join AFZ_Visitors AV on AA.Visitor_ID = AV.Visitor_ID
-        join AFZ_Facility AF on AST.Source_Type = AF.Source_Type
-        join AFZ_Shows SHOWS on AF.Facility_ID = SHOWS.Facility_ID
-        order by Visitor_ID
-END
+        left JOIN AFZ_Source_Type AST on AA.Source_Type = AST.Source_Type
+        left join AFZ_Facility AF on AF.Facility_ID = AA.Facility_ID
+        left join AFZ_Shows SHOWS on AF.Facility_ID = SHOWS.Facility_ID
+        left join AFZ_Ticket_Type ATT on AT.Ticket_Type_ID = ATT.Ticket_Type_ID
+        left join AFZ_Ticket_Method ATM on AT.Method_Type_ID = ATM.Method_Type_ID
+        JOIN AFZ_Visitors AV on AA.Visitor_ID = AV.Visitor_ID
+        where cast(AA.Activity_Date as Date) = @input_date
+    END
+
 
 EXECUTE get_summary_data_by_date '2023-04-03';
 
