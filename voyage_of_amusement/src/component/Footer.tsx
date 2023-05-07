482592ba0809bc8eb2aea2c79da06eaeb099bd43
Yunze Zhao <zhao.yunzeabh@gmail.com>
2023-05-07 01:37:49 -0400

    fix footer

--- voyage_of_amusement/src/component/Footer.tsx
+++ voyage_of_amusement/src/component/Footer.tsx
@@ -1,18 +1,30 @@
-import React from 'react';
-import Link from 'next/link';
+import React from "react";
+import Link from "next/link";
 
 const Footer: React.FC = () => {
   return (
     <footer className="flex items-center justify-center w-full h-16 border-t fixed bottom-0 bg-slate-50">
       <div className="flex flex-col items-center">
         <small className="text-gray-500 mb-2">
-          Made with <span role="img" aria-label="heart">❤️</span> with
-          <Link  className="ml-1 text-blue-500 underline hover:text-blue-600" href="https://nextjs.org/" passHref>
+          Crafted with{" "}
+          <span role="img" aria-label="heart">
+            ❤️
+          </span>{" "}
+          using
+          <Link
+            className="ml-1 text-blue-500 underline hover:text-blue-600"
+            href="https://nextjs.org/"
+            passHref
+          >
             Next.js
           </Link>
         </small>
         <small className="text-gray-500 flex items-center">
-          By students at NYU Tandon School of Engineering <span role="img" aria-label="NYU logo">🎓</span>, NYC
+          By students at NYU Tandon School of Engineering{" "}
+          <span role="img" aria-label="NYU logo">
+            🎓
+          </span>
+          , NYC
         </small>
       </div>
     </footer>
